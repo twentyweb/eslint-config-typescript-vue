ae0e39eed5bf94d17e4b85e84d2a7ad2fca9e44b
Fabio Capucci <fabio.cappuc@gmail.com>
2019-08-17 15:21:34 +0200

    Upgraded typescript-eslint
    Replaced deprecated rules with new ones
    Updated min version of dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     },
     parserOptions: {
         "parser": "@typescript-eslint/parser",
+        "extraFileExtensions": [".vue"],
     },
     extends: [
         'plugin:@typescript-eslint/recommended',
@@ -18,27 +19,33 @@ module.exports = {
         "vue/eqeqeq": ["error"],
         "vue/html-indent": ["error", 4],
         "vue/max-attributes-per-line": ["error", {
-            "multiline": {
-                "allowFirstLine": true,
+            multiline: {
+                allowFirstLine: true,
             },
         }],
         // "vue/no-deprecated-scope-attribute": ["error"],
         "@typescript-eslint/explicit-member-accessibility": ["error", {
-            "accessibility": "no-public",
+            accessibility: "no-public",
         }],
-        "@typescript-eslint/no-object-literal-type-assertion": ["error"],
+        "@typescript-eslint/consistent-type-assertions": ["error", {
+            assertionStyle: 'as',
+            objectLiteralTypeAssertions: 'never',
+        }],
+        "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
         "@typescript-eslint/explicit-function-return-type": ["warn", {
-            "allowExpressions": true,
+            allowExpressions: true,
         }],
         "@typescript-eslint/no-explicit-any": ["warn", {
             // "fixToUnknown": true,
-            "ignoreRestArgs": true,
+            ignoreRestArgs: true,
         }],
         "@typescript-eslint/camelcase": ["off"],
+        "@typescript-eslint/no-inferrable-types": ["off"],
         "@typescript-eslint/interface-name-prefix": ["off"],
         "@typescript-eslint/no-non-null-assertion": ["warn"],
         "@typescript-eslint/no-unused-vars": ["warn", {
             "ignoreRestSiblings": true,
         }],
+        "@typescript-eslint/ban-ts-ignore": ["warn"],
     },
 };
