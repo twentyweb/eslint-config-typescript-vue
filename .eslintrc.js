84d6778f4d926c3384b14b980b6551ef5246d1bb
Fabio Capucci <fabio.cappuc@gmail.com>
2020-05-25 10:39:50 +0200

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
         browser: true,
     },
     parserOptions: {
-        "parser": "@typescript-eslint/parser",
-        "extraFileExtensions": [".vue"],
+        'parser': '@typescript-eslint/parser',
+        'extraFileExtensions': ['.vue'],
     },
     extends: [
         'plugin:@typescript-eslint/recommended',
@@ -15,37 +15,39 @@ module.exports = {
         'vue',
     ],
     rules: {
-        "eqeqeq": ["error"],
-        "vue/eqeqeq": ["error"],
-        "vue/html-indent": ["error", 4],
-        "vue/max-attributes-per-line": ["error", {
+        'eqeqeq': ['error'],
+        'vue/eqeqeq': ['error'],
+        'vue/html-indent': ['error', 4],
+        'vue/max-attributes-per-line': ['error', {
             multiline: {
                 allowFirstLine: true,
             },
         }],
         // "vue/no-deprecated-scope-attribute": ["error"],
-        "@typescript-eslint/explicit-member-accessibility": ["error", {
-            accessibility: "no-public",
+        '@typescript-eslint/explicit-member-accessibility': ['error', {
+            accessibility: 'no-public',
         }],
-        "@typescript-eslint/consistent-type-assertions": ["error", {
+        '@typescript-eslint/consistent-type-assertions': ['error', {
             assertionStyle: 'as',
             objectLiteralTypeAssertions: 'never',
         }],
-        "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
-        "@typescript-eslint/explicit-function-return-type": ["warn", {
+        '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+        '@typescript-eslint/explicit-function-return-type': ['warn', {
             allowExpressions: true,
         }],
-        "@typescript-eslint/no-explicit-any": ["warn", {
+        '@typescript-eslint/no-explicit-any': ['warn', {
             // "fixToUnknown": true,
             ignoreRestArgs: true,
         }],
-        "@typescript-eslint/camelcase": ["off"],
-        "@typescript-eslint/no-inferrable-types": ["off"],
-        "@typescript-eslint/interface-name-prefix": ["off"],
-        "@typescript-eslint/no-non-null-assertion": ["warn"],
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            "ignoreRestSiblings": true,
+        '@typescript-eslint/camelcase': ['off'],
+        '@typescript-eslint/no-inferrable-types': ['off'],
+        '@typescript-eslint/interface-name-prefix': ['off'],
+        '@typescript-eslint/no-non-null-assertion': ['warn'],
+        '@typescript-eslint/no-unused-vars': ['warn', {
+            'ignoreRestSiblings': true,
         }],
-        "@typescript-eslint/ban-ts-ignore": ["warn"],
+        '@typescript-eslint/ban-ts-comment': ['warn'],
+        // '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        // '@typescript-eslint/ban-types': ['off'],
     },
 };
