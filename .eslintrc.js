b6c1a2d239980acadb32a784ecd74a9f8fbbf339
Fabio Capucci <fabio.cappuc@gmail.com>
2023-01-05 10:29:07 +0100

    updated config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,10 @@ module.exports = {
         browser: true,
     },
     parserOptions: {
-        'parser': '@typescript-eslint/parser',
-        'extraFileExtensions': ['.vue'],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.vue'],
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
     },
     extends: [
         'plugin:@typescript-eslint/recommended',
@@ -15,16 +17,16 @@ module.exports = {
         'vue',
     ],
     rules: {
-        'eqeqeq': ['error'],
-        'vue/eqeqeq': ['error'],
+        'eqeqeq': ['off'],
+        'vue/eqeqeq': ['off'],
         'vue/html-indent': ['error', 4],
         'vue/max-attributes-per-line': ['error', {
             singleline: 3,
             multiline: 1,
         }],
         'vue/first-attribute-linebreak': ['error', {
-            'singleline': 'beside',
-            'multiline': 'beside',
+            singleline: 'beside',
+            multiline: 'beside',
         }],
         'vue/multi-word-component-names': ['off'],
         '@typescript-eslint/explicit-member-accessibility': ['error', {
@@ -35,19 +37,22 @@ module.exports = {
             objectLiteralTypeAssertions: 'never',
         }],
         '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+        '@typescript-eslint/consistent-type-exports': ['error', {
+            fixMixedExportsWithInlineTypeSpecifier: false,
+        }],
+        '@typescript-eslint/consistent-type-imports': ['error', {
+            prefer: 'type-imports',
+        }],
         '@typescript-eslint/explicit-function-return-type': ['warn', {
             allowExpressions: true,
         }],
-        '@typescript-eslint/no-explicit-any': ['warn', {
-            // "fixToUnknown": true,
-            ignoreRestArgs: true,
-        }],
+        '@typescript-eslint/no-explicit-any': ['off'],
         '@typescript-eslint/camelcase': ['off'],
         '@typescript-eslint/no-inferrable-types': ['off'],
         '@typescript-eslint/interface-name-prefix': ['off'],
         '@typescript-eslint/no-non-null-assertion': ['warn'],
         '@typescript-eslint/no-unused-vars': ['warn', {
-            'ignoreRestSiblings': true,
+            ignoreRestSiblings: true,
         }],
         '@typescript-eslint/ban-ts-comment': ['warn'],
     },
